27743db3459ad987186eb7fae28a44d4abcc6827
Ian Chua <ianchuajl@hotmail.com>
2023-09-10 13:55:38 +1200

    minor change to JS, deleted stuff that was not used

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const displaySideNav = () => {
 }
 
 window.addEventListener('scroll', displaySideNav)
-window.addEventListener('resize', displaySideNav)
 
 // Variable Year //
 const year = document.getElementById('year')
@@ -24,24 +23,3 @@ const thisYear = new Date().getFullYear()
 year.setAttribute('datetime', thisYear)
 year.textContent = thisYear
 
-// FROM AN EXAMPLE //
-// This MIGHT come in useful but not for what I'm after //
-// const displaySideNav = () => {
-//   const nav = document.getElementById('side-nav')
-//   const anchor = document.getElementById('side-nav-link')
-//   const screenWidth = window.innerWidth || document.documentElement.clientWidth
-
-//   if (screenWidth >= 1024) {
-//     this.scrollY >= 135
-//       ? nav.classList.add('show-nav')
-//       : nav.classList.remove('show-nav')
-//   } else if (screenWidth >= 768) {
-//     this.scrollY >= 130
-//       ? (nav.classList.add('show-med-nav'), anchor.classList.add('side-menu-link2'))
-//       : (nav.classList.remove('show-med-nav'), anchor.classList.remove('side-menu-link2'))
-//   } else {
-//     nav.classList.remove('show-nav')
-//   }
-// }
-
-// window.addEventListener('scroll', displaySideNav)
